43d1281d44a6c43858bd2b1d3e7b1cf75cdb21cf
SaurabhSuman1607 <saurabhsuman1607@gmail.com>
2022-09-24 01:06:35 +0530

    added features

--- src/component/List.js
+++ src/component/List.js
@@ -2,10 +2,56 @@ import React, { Component } from 'react'
 import {movies} from './getMovies'
 export default class 
 extends Component {
+   constructor(){
+    
+    super();
+    this.state={
+      hover:"",
+    };
+   }
+
+  
+  handleEnter=(id)=> {
+    this.setState({
+     hover:id,
+    });
+
+   }
+   
+   handleLeave=() => {
+     this.setState({
+       hover:"",
+     })
+   }  
+
+ async componentDidMount(){
+  console.log("CDM used");
+  let res=await fetch("https://api.themoviedb.org/3/movie/popular?api_key=6c10a4f6614e34d94d7b489786dfcd43&language=en-US&page=1");
+  let data=await res.json();
+  console.log(data);
+ }
+ componentDidUpdate(){
+ console.log("CDU used");
+
+ }
+ componentWillUnmount(){
+ console.log("CWU usedReact");
+
+ }
+
+
+
   render() {
     let AllMovies=movies.results;
     return (
-      <div>
+      <>
+      {AllMovies.length==0?
+      (
+<div class="spinner-border text-info" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>):
+      
+      (<div>
         <h3 className='trending'>
             Trending
         </h3>
@@ -13,15 +59,20 @@ extends Component {
         {
              AllMovies.map((movieobj) => {
                 return( 
-                    <div className="card movie-card" >
+                    <div className="card movie-card"
+                    onMouseEnter={()=>this.handleEnter(movieobj.id)}
+                    onMouseLeave={this.handleLeave} 
+                     >
                     <img src={`https://image.tmdb.org/t/p/original/${movieobj.backdrop_path}`} class="card-img-top movie-img" alt="..." />
                     
                       <h5 class="card-title movie-title">{movieobj.original_title}</h5>
                       {/* <p class="card-text movie-text">{movie.overview}</p> */}
                       <div className='button-wrapper'>
-                     <a href="#" class="btn btn-primary movie-button">
+                     { this.state.hover == movieobj.id && (
+                     < a href="#" className="btn btn-primary movie-button">
                      Add to Favourites
                      </a>
+                      )}
                      </div>
                     
                   </div>
@@ -33,6 +84,20 @@ extends Component {
       
    </div>
   </div>
-    )
+  
+  //   <nav aria-label="Page navigation example">
+  //   <ul class="pagination">
+  //     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">1</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">2</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">3</a></li>
+  //     <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  //   </ul>
+  // </nav>
+  
+  )}
+
+  </> 
+      )
   }
 }
